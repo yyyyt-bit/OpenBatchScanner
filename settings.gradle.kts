e92d9dba47d996da89a05f1bd710934680af50a3
yyyyt-bit <dara72454@gmail.com>
2026-09-24 17:32:17 -0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositories { google(); mavenCentral() } }
+rootProject.name = "BatchScanner"
+include(":app")
